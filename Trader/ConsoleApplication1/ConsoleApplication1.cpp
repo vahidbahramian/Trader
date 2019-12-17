e263afad05447ac3690ab72fa5e84e6dd9cb1e1f
vahid <vahid.bahramian92@gmail.com>
2019-12-17 21:46:52 +0330

    refactor with vector

--- Trader/ConsoleApplication1/ConsoleApplication1.cpp
+++ Trader/ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,18 +8,8 @@
 #include <cstring>
 #include <time.h>
 #include <chrono>
+#include <thread>
 
-
-
-
-
-/*double open[10000]; double high[10000]; double low[10000]; double close[10000];
-int volume[10000]; double s10[10000]; double s20[10000]; double s30[10000];
-double s40[10000];  double s50[10000]; double s60[10000]; double s70[10000];
-bool osma[10000]; bool macdm[10000]; bool ma20[10000]; bool rsi[10000];
-bool cci[10000]; bool pin_bar[10000]; bool osma_h4[10000]; bool macdm_h4[10000];
-bool rsi_h4[10000]; bool cci_h4[10000]; bool pin_bar_h4[10000]; bool osma_d1[10000]; bool macdm_d1[10000];
-bool ma20_d1[10000]; bool rsi_d1[10000]; bool cci_d1[10000]; bool pin_bar_d1[10000];*/
 bool read_from_excel_file(std::string file,int numberOfColumn, std::vector<std::vector<double>>& cell)
 {
 	std::string line;
@@ -43,83 +33,145 @@ bool read_from_excel_file(std::string file,int numberOfColumn, std::vector<std::
 	}
 	return true;
 }
-
+std::vector<std::vector<int>> count_one_in_each_column(const std::vector<std::vector<double>>& data, int begin, int end)
+{
+	std::vector<int> temp;
+	std::vector<std::vector<int>> data_out;
+	for (int i = begin; i <= end; i++)
+	{
+		for (int j = 0; j < data.size(); j++)
+			if (data[j][i] == 1)
+			{
+				temp.push_back(j);
+			}
+		data_out.push_back(temp);
+		temp.clear();
+	}
+	return data_out;
+}
+std::vector<std::vector<int>> count_if_greater_than_i_for_each_column(const std::vector<std::vector<double>>& data,
+	const std::vector<std::vector<int>>& index_of_data, float factor, int j)
+{
+	std::vector<int> temp;
+	temp.clear();
+	std::vector<std::vector<int>> data_out;
+	for (int i = 0; i < index_of_data.size(); i++)
+	{
+		for(int k : index_of_data[i])
+			if (data[k][j] >= factor)
+			{
+				temp.push_back(k);
+			}
+		data_out.push_back(temp);
+		temp.clear();
+	}
+	return data_out;
+}
+std::vector<std::vector<int>> index_of_two_one_in_equal_index(const std::vector<std::vector<int>>& index_of_data, int k)
+{
+	std::vector<int> temp;
+	std::vector<std::vector<int>> data_out;
+	for (int i = k; i < index_of_data.size(); i++)
+	{
+	//	for (int j = i; j <= index_of_data.size() - 1; j++)
+	//	{
+		if (index_of_data[k].size() <= index_of_data[i].size())
+		{
+			for (int j : index_of_data[k])
+			{
+				auto it = std::find(index_of_data[i].begin(), index_of_data[i].end(), j);
+				if (it != index_of_data[i].end())
+					temp.push_back(j);
+			}
+			data_out.push_back(temp);
+			temp.clear();
+		}
+		else
+		{
+			for (int j : index_of_data[i])
+			{
+				auto it = std::find(index_of_data[k].begin(), index_of_data[k].end(), j);
+				if (it != index_of_data[k].end())
+					temp.push_back(j);
+			}
+			data_out.push_back(temp);
+			temp.clear();
+		}
+	//	}
+	}
+	return data_out;
+}
 int main() {
 	//time_t start, end;
 	//time(&start);
+	//std::thread t1(task1, "Hello");
 	auto started = std::chrono::high_resolution_clock::now();
 	std::vector<std::vector<double>> data;
 	std::vector<std::vector<double>> show(20);
 	double max[20] = { -10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10 };
 
 	read_from_excel_file("saeed.txt", 32, data);
-	float c; float cc;
-	float b; float bb;
+
 	int jj;
-
-	char* pch;
-
+	double temp;
 	for (int i = 0; i < 20; i++)
 		show[i] = std::vector<double>(6);
 
-	for (float i = 1.1; i <= 2.1; i += 0.5)
-		for (int j = 5; j <= 11; j++)
-			for (int k = 12; k <= 28; k++)
-			{
-				c = 0; b = 0;
-				for (int l = 0; l <= 9831; l++) {
-					if (data[l][k] == 1)
-					{
-						c++;
-						if (data[l][j] >= i)
-							b++;
-					}
+	std::vector<std::vector<int>> c = count_one_in_each_column(data, 12, 28);
+	std::vector<std::vector<int>> cc;
+	std::vector<std::vector<int>> b;
+	std::vector<std::vector<int>> bb;
 
-				}
+	for (float i = 1.1; i <= 2.1; i += 0.5)
+	{
+		for (int j = 0; j <= 6; j++)
+		{
+			b = count_if_greater_than_i_for_each_column(data, c, i,j+5);
+			for (int k = 0; k <= 16; k++)
+			{
+				if (c[k].size() != 0)
+					temp = (i * 0.02 * ((float)b[k].size() / (float)c[k].size())) - (0.02 * (1 - ((float)b[k].size() / (float)c[k].size())));
 				for (int l = 0; l < 20; l++) {
-					double temp = (i * 0.02 * (b / c)) - (0.02 * (1 - (b / c)));
 					if (temp > max[l]) {
 						max[l] = temp;
-						show[l][0] = c;
-						show[l][1] = b;
-						show[l][2] = (b / c)*100;
-						show[l][3] = j;
+						show[l][0] = c[k].size();
+						show[l][1] = b[k].size();
+						show[l][2] = ((float)b[k].size() / (float)c[k].size()) * 100;
+						show[l][3] = j + 5;
 						show[l][4] = i;
-						show[l][5] = max[l]*100;
+						show[l][5] = max[l] * 100;
 						break;
 					}
 				}
-				;
-				for (jj = k; jj <= 28; jj++)
+				cc = index_of_two_one_in_equal_index(c,k);
+				bb = count_if_greater_than_i_for_each_column(data, cc, i, j+5);
+				for (jj = k; jj <= 16; jj++)
 				{
-					cc = 0;
-					bb = 0;
-					for (int l = 0; l <= 9831; l++) {
-						if (data[l][k] == 1 && data[l][jj] == 1)
-						{
-							cc++;
-							if (data[l][j] >= i)
-								bb++;
-						}
-
-					}
+					int idx = jj - k;
+					if (cc[idx].size() != 0)
+						temp = (i * 0.02 * ((float)bb[idx].size() / (float)cc[idx].size())) - (0.02 * (1 - ((float)bb[idx].size() / (float)cc[idx].size())));
 					for (int l = 0; l < 20; l++) {
-						if (cc == 0)
-							break;
-						double temp = (i * 0.02 * (bb / cc)) - (0.02 * (1 - (bb / cc)));
 						if (temp > max[l]) {
 							max[l] = temp;
-							show[l][0] = cc;
-							show[l][1] = bb;
-							show[l][2] = (bb / cc)*100;
-							show[l][3] = j;
+							show[l][0] = cc[idx].size();
+							show[l][1] = bb[idx].size();
+							show[l][2] = ((float)bb[idx].size() / (float)cc[idx].size()) * 100;
+							show[l][3] = j+5;
 							show[l][4] = i;
-							show[l][5] = max[l]*100;
+							show[l][5] = max[l] * 100;
 							break;
 						}
 					}
 				}
+				bb.clear();
+				bb.shrink_to_fit();
+				cc.clear();
+				cc.shrink_to_fit();
 			}
+			b.clear();
+			b.shrink_to_fit();
+		}
+	}
 	//time(&end);
 	auto done = std::chrono::high_resolution_clock::now();
 	//double execute_time_min = double(end - start);
